b27365385ff8fea6c11dc466d57b97e12c673884
Baurzhan <zetbaur@gmail.com>
2023-04-20 11:19:27 +0600

    color pallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
 import { MainLayout, AuthLayout } from './components';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { ColorModeContext, useMode } from './theme';
 
 function App() {
-  return (
-    <div className='app'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<MainLayout />}>
-            <Route index element={<Dashboard />} />
-          </Route>
+  const [theme, colorMode] = useMode();
 
-          <Route path='login' element={<AuthLayout />} />
-          <Route path='register' element={<AuthLayout />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <div className='app'>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<MainLayout />}>
+                <Route index element={<Dashboard />} />
+              </Route>
+
+              <Route path='login' element={<AuthLayout />} />
+              <Route path='register' element={<AuthLayout />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 

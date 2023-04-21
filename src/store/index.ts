8dfc2b80d74e33c9dacdcae82cc9701c201eb46a
Baurzhan <zetbaur@gmail.com>
2023-04-21 12:15:03 +0600

    axios instance

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import themeSlice from './features/theme.slice';
+import themeSlice from './features/theme/themeSlice';
 
 export const store = configureStore({
   reducer: {

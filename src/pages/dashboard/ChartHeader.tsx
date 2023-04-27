bf91fefcdc9966ca4c85fab4b58277dbac10feb4
Baurzhan <zetbaur@gmail.com>
2023-04-27 11:29:29 +0600

    radio group setup

--- src/pages/dashboard/ChartHeader.tsx
+++ src/pages/dashboard/ChartHeader.tsx
@@ -8,6 +8,10 @@ import {
   Select,
   Autocomplete,
   TextField,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
 } from '@mui/material';
 import { coinList } from '../../data/coinList';
 
@@ -18,6 +22,7 @@ import {
   setVsCurrency,
   setInterval,
 } from '../../store/features/coins/coinsSlice';
+import SelectDay from './SelectDay';
 
 const HistoricChartHeader = () => {
   const theme = useTheme();
@@ -33,133 +38,148 @@ const HistoricChartHeader = () => {
     <Box
       sx={{
         padding: '1rem',
-        display: 'flex',
-        gap: '1rem',
-        alignItems: 'center',
       }}
     >
       <Box
         sx={{
           display: 'flex',
+          gap: '1rem',
           alignItems: 'center',
-          gap: '8px',
-          minWidth: '100px',
-          marginRight: '1 rem',
-          background: colors.secondary.DEFAULT,
-          borderRadius: '4px',
-          padding: '8px 16px',
+          marginBottom: '1rem',
         }}
       >
-        <img src={currentCoin?.image} alt={currentCoin?.symbol} />
-        <span>{currentCoin?.name}</span>
-      </Box>
-
-      <Box
-        component='form'
-        sx={{
-          background: theme.palette.background.paper,
-          display: 'flex',
-          borderRadius: '4px',
-          flex: 1,
-        }}
-      >
-        <Autocomplete
-          size='small'
-          fullWidth
-          freeSolo
-          options={coinList
-            .map((option) => option.id)
-            .filter((item) => item.length < 11)}
-          value={id}
-          onChange={(event, newCoin: string | null) => dispatch(setId(newCoin))}
+        <Box
           sx={{
-            background: '#000000',
-          }}
-          renderInput={(params) => (
-            <TextField {...params} placeholder='Search Coin' />
-          )}
-        />
-      </Box>
-
-      <FormControl size='small' variant='outlined'>
-        <Select
-          value={vsCurrency}
-          onChange={(e) => dispatch(setVsCurrency(e.target.value))}
-          sx={{
-            width: '100px',
-            '& .MuiSvgIcon-root': {
-              fill: 'yellow',
-            },
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px',
+            minWidth: '100px',
+            marginRight: '1 rem',
+            background: colors.secondary.DEFAULT,
+            borderRadius: '4px',
+            padding: '8px 16px',
           }}
         >
-          {currencies.map((el) => {
-            return (
-              <MenuItem key={el.code} value={el.code}>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    gap: '8px',
-                  }}
-                >
+          <img src={currentCoin?.image} alt={currentCoin?.symbol} />
+          <span>{currentCoin?.name}</span>
+        </Box>
+
+        <Box
+          component='form'
+          sx={{
+            background: theme.palette.background.paper,
+            display: 'flex',
+            borderRadius: '4px',
+            flex: 1,
+          }}
+        >
+          <Autocomplete
+            size='small'
+            fullWidth
+            freeSolo
+            options={coinList
+              .map((option) => option.id)
+              .filter((item) => item.length < 11)}
+            value={id}
+            onChange={(event, newCoin: string | null) =>
+              dispatch(setId(newCoin))
+            }
+            sx={{
+              background: '#000000',
+            }}
+            renderInput={(params) => (
+              <TextField {...params} placeholder='Search Coin' />
+            )}
+          />
+        </Box>
+
+        <FormControl size='small' variant='outlined'>
+          <Select
+            value={vsCurrency}
+            onChange={(e) => dispatch(setVsCurrency(e.target.value))}
+            sx={{
+              width: '100px',
+              '& .MuiSvgIcon-root': {
+                fill: 'yellow',
+              },
+            }}
+          >
+            {currencies.map((el) => {
+              return (
+                <MenuItem key={el.code} value={el.code}>
                   <Box
                     sx={{
-                      color: '#4688E4',
+                      display: 'flex',
+                      gap: '8px',
                     }}
                   >
-                    {el.symbolNative}
+                    <Box
+                      sx={{
+                        color: '#4688E4',
+                      }}
+                    >
+                      {el.symbolNative}
+                    </Box>
+
+                    <Box>{el.code}</Box>
                   </Box>
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </Box>
 
-                  <Box>{el.code}</Box>
-                </Box>
-              </MenuItem>
-            );
-          })}
-        </Select>
-      </FormControl>
+      <Box>
+        <SelectDay />
 
-      <FormControl size='small' variant='outlined'>
-        <Select
-          value={days}
-          onChange={(e) => dispatch(setDays(e.target.value))}
-          sx={{
-            minWidth: '120px',
-            '& .MuiSvgIcon-root': {
-              fill: 'yellow',
-            },
-          }}
-        >
-          <MenuItem value='1'>Last Day</MenuItem>
-          <MenuItem value='7'>Last Week</MenuItem>
-          <MenuItem value='30'>Last Month</MenuItem>
-          <MenuItem value='90'>Last 3 Months</MenuItem>
+        {/* <FormControl size='small' variant='outlined'>
+          <Select
+            value={days}
+            onChange={(e) => dispatch(setDays(e.target.value))}
+            sx={{
+              minWidth: '120px',
+              '& .MuiSvgIcon-root': {
+                fill: 'yellow',
+              },
+            }}
+          >
+            <MenuItem value='1'>Last Day</MenuItem>
+            <MenuItem value='7'>Last Week</MenuItem>
+            <MenuItem value='30'>Last Month</MenuItem>
+            <MenuItem value='90'>Last 3 Months</MenuItem>
 
-          <MenuItem value='180' disabled={interval === 'hourly'}>
-            Last 6 Months
-          </MenuItem>
+            <MenuItem value='180' disabled={interval === 'hourly'}>
+              Last 6 Months
+            </MenuItem>
 
-          <MenuItem value='360' disabled={interval === 'hourly'}>
-            Last year
-          </MenuItem>
-        </Select>
-      </FormControl>
+            <MenuItem value='360' disabled={interval === 'hourly'}>
+              Last year
+            </MenuItem>
+          </Select>
+        </FormControl>
 
-      <FormControl size='small' variant='outlined'>
-        <Select
-          value={interval}
-          onChange={(e) => dispatch(setInterval(e.target.value))}
-          sx={{
-            width: '100px',
-            '& .MuiSvgIcon-root': {
-              fill: 'yellow',
-            },
-          }}
-        >
-          <MenuItem value='hourly' disabled={days === '180' || days === '360'}>
-            Hourly
-          </MenuItem>
-          <MenuItem value='daily'>Dayly</MenuItem>
-        </Select>
-      </FormControl>
+        <FormControl size='small' variant='outlined'>
+          <Select
+            value={interval}
+            onChange={(e) => dispatch(setInterval(e.target.value))}
+            sx={{
+              width: '100px',
+              '& .MuiSvgIcon-root': {
+                fill: 'yellow',
+              },
+            }}
+          >
+            <MenuItem
+              value='hourly'
+              disabled={days === '180' || days === '360'}
+            >
+              Hourly
+            </MenuItem>
+            <MenuItem value='daily'>Dayly</MenuItem>
+          </Select>
+        </FormControl> */}
+      </Box>
     </Box>
   );
 };
